eee542440ad65eb4ef9df8be046b9abd409ddb85
MineGame159 <petulko08@gmail.com>
2026-07-13 20:21:29 +0200

    Include folder structure in object file names

--- src/build/utils.go
+++ src/build/utils.go
@@ -9,11 +9,16 @@ import (
 var nameReplacer = strings.NewReplacer(" ", "_", "-", "_")
 
 func getBuildFileName(file *project.File) string {
-	name := filepath.Base(file.Path)
+	name, err := filepath.Rel(filepath.Join(file.Proj.Path, "src"), file.Path)
+	if err != nil {
+		panic("build.getBuildFileName() - Failed to get src relative file path: " + err.Error())
+	}
 
 	if index := strings.IndexRune(name, '.'); index != -1 {
 		name = name[:index]
 	}
+
+	name = strings.ReplaceAll(name, string(filepath.Separator), "___")
 
 	return nameReplacer.Replace(name)
 }
